b7d73d438000c72d1de270ab5fb24b977181061e
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-02-24 13:22:17 -0600

    final project. the lines don't line up but its okay

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ namespace Variables_DeBord_Joshua
         static void Main(string[] args)
         {
             
-            int favoriteNumber;
-            bool isJumping = false, isRunning = false;
-            double applePie;
-            applePie = 9;
-            favoriteNumber = 7;
-            const double finalGrade = 69420;
-            Console.WriteLine($"My favorite number is {favoriteNumber}.");
-            Console.WriteLine($"{isJumping}");
-            Console.WriteLine($"{isRunning}");
-            Console.WriteLine($"{applePie}");
-            Console.WriteLine($"{finalGrade}");
+            int favoriteNumber; //this is a integer and is called favoriteNumber
+            bool isJumping = false, isRunning = false; //this states that the user is not jumping or running when put in this WriteLine
+            double applePie; //this is a floating point, also called a double. This floating point is called applePie
+            applePie = 9; //this tells the system that the floating point "applePie" is 9.
+            favoriteNumber = 7; //the explains that the creator's favorite number is 7. Now when ever the varaible is used in a WriteLine, it will show as 7.
+            const double finalGrade = 69420; //this is a constant floating point. It is named "finalGrade". Because it is constant, it will always be 69420.
+            Console.WriteLine($"My favorite number is {favoriteNumber}."); //This makes the Console write information to the user.
+            Console.WriteLine($"{isJumping}"); //This makes the Console write information to the user.
+            Console.WriteLine($"{isRunning}"); //This makes the Console write information to the user.
+            Console.WriteLine($"{applePie}"); //This makes the Console write information to the user.
+            Console.WriteLine($"{finalGrade}"); //This makes the Console write information to the user.
         } 
     }
 }
